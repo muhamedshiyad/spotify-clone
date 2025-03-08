e8b6337040b75f68c8df91a27e64dd7194eff24f
muhamedshiyad <muhamedshiyad18@gmail.com>
2025-03-08 16:13:48 +0530

    Main Layout and Left Sidebar

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -1,7 +1,15 @@
 import { axiosInstance } from "@/lib/axios";
 import {create} from "zustand";
 
-export const useMusicStore = create((set) => ({
+interface MusicStore {
+    songs: any[];
+    albums: any[];
+    isLoading: boolean;
+
+    fetchAlbums: () => Promise<void>;
+}
+
+export const useMusicStore = create<MusicStore>((set) => ({
     albums:[],
     songs:[],
     isLoading: false,
