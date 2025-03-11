c6ecefa4305e2fb6eb55f206fa0e5c7780689066
muhamedshiyad <muhamedshiyad18@gmail.com>
2025-03-11 14:15:28 +0530

    Creating Album Page

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -6,26 +6,42 @@ interface MusicStore {
     songs: Song[];
     albums: Album[];
     isLoading: boolean;
+	error: string | null;
+	currentAlbum: Album | null;
 
     fetchAlbums: () => Promise<void>;
+	fetchAlbumById:(id:string) => Promise<void>;
 }
 
 export const useMusicStore = create<MusicStore>((set) => ({
     albums:[],
     songs:[],
     isLoading: false,
-    error: null,
+	error: null,
+	currentAlbum: null,
 
     fetchAlbums: async () => {
-        set({isLoading: true, error: null });
-        
-        try {
-            const response = await axiosInstance.get("/albums");
-            set({albums:response.data})
-        } catch (error:any) {
-            set({error:error.response.data.message});
-        }finally{
-            set({isLoading: false});
-        }
-    },
+		set({ isLoading: true, error: null });
+
+		try {
+			const response = await axiosInstance.get("/albums");
+			set({ albums: response.data });
+		} catch (error: any) {
+			set({ error: error.response.data.message });
+		} finally {
+			set({ isLoading: false });
+		}
+	},
+
+	fetchAlbumById:async(id:string) =>{
+		set({isLoading: true,error:null});
+		try {
+			const response = await axiosInstance.get(`/albums/${id}`);
+			set({currentAlbum:response.data});
+		} catch (error) {
+			set({error: error.response.data.message});
+		}finally{
+			set({isLoading: false});
+		}
+	}
 }));
