c4747589e3f86c01ba94d4bb2f2c224d2924e2bf
muhamedshiyad <muhamedshiyad18@gmail.com>
2025-03-24 14:07:18 +0530

    Play Song Functionality & Queue

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,13 +1,15 @@
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import { Outlet } from "react-router-dom";
 import LeftSidebar from "./components/LeftSidebar";
-import FriendsActivity from "./components/friendsActivity";
+import FriendsActivity from "./components/FriendsActivity";
+import AudioPlayer from "./components/AudioPlayer";
 
 const MainLayout = () => {
     const isMobile = false;
   return <div className="h-screen bg-black text-white flex flex-col">
 
       <ResizablePanelGroup direction="horizontal" className="flex flex-1 h-full overflow-hidden p-2">
+        <AudioPlayer/>
         {/*left sidebar */}
         <ResizablePanel defaultSize={20} minSize ={isMobile ? 0 : 10} maxSize={30}>
             <LeftSidebar/> 
