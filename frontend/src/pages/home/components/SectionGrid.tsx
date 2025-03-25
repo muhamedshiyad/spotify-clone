5e9d9c7a7a64116e2ed1296c08277b453d43812d
muhamedshiyad <muhamedshiyad18@gmail.com>
2025-03-25 15:21:32 +0530

    Play Button Component

--- frontend/src/pages/home/components/SectionGrid.tsx
+++ frontend/src/pages/home/components/SectionGrid.tsx
@@ -1,6 +1,7 @@
 import SectionGridSkeleton from "@/components/skeletons/SectionGridSkeleton";
 import { Button } from "@/components/ui/button";
 import { Song } from "@/types";
+import PlayButton from "./PlayButton";
 
 type SectionGridProps = {
   title: string,
@@ -31,8 +32,8 @@ const SectionGrid = ({title,songs,isLoading}:SectionGridProps) => {
                 className="w-full h-full object-cover transition-transform duration-300 
                 group-hover:scale-105"
                 />
-                {/*TODO play button*/}
               </div>
+              <PlayButton song={song}/>
             </div>
             <h3 className="font-medium mb-2 truncate">{song.title}</h3>
             <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
